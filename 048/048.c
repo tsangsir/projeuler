99147db1e5ba6cda32e884b577f18553e1e9691e
tsangsir <thtsang@linuxmail.org>
2020-01-21 06:07:26 +0000

    bug fix... uninitialized variable :(

--- 048/048.c
+++ 048/048.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char * argv[]) {
-	long int result1=0, result2, tmp1, tmp2;
+	unsigned int result1=0, result2=0, tmp1, tmp2;
 	int i, j;
 
 	for (i=1; i<=1000;i++) {
@@ -16,7 +16,7 @@ int main(int argc, char * argv[]) {
 			if (tmp1 > 100000) {
 				tmp2 += (int)(tmp1 / 100000);
 				tmp1 = tmp1 % 100000;
-				printf("(%ld, %ld)", tmp2, tmp1);
+				printf("(%u, %u)", tmp2, tmp1);
 			}
 		}
 		printf("%d: %u, %u - ", i, tmp2, tmp1);
@@ -26,7 +26,7 @@ int main(int argc, char * argv[]) {
 			result2 += (int)(result1 / 100000);
 			result1 = result1 % 100000;
 		}
-		printf("result: %ld, %ld\n", result1, result2);
+		printf("result: %u, %u\n", result1, result2);
 	}
-	printf("%ld, %ld\n", result2, result1);
+	printf("%u, %u\n", result2, result1);
 }
